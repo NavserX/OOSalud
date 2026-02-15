81cc64b1a7227f082b0ae02ad1d4cecb081e64b5
NavserX <snavarroazorin@gmail.com>
2026-02-15 21:44:14 +0100

    Modificado el PacienteModelo con busquedas en base de datos

--- src/app/Modelo/PacienteModelo.php
+++ src/app/Modelo/PacienteModelo.php
@@ -2,13 +2,15 @@
 
 namespace App\Modelo;
 
+use App\Class\Paciente;
+use App\Controlador\PacienteControlador;
 use PDO;
 use PDOException;
 
 class PacienteModelo
 {
 
-    public static function ObtenerPaciente($id):array{
+    /*public static function ObtenerPaciente($id):array{
         try{
             $conexion = new \PDO("mysql:host=mariadb;dbname=examen", "alumno", "alumno");
             $conexion->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
@@ -22,6 +24,21 @@ class PacienteModelo
         $resultado = $stmt->fetch(PDO::FETCH_ASSOC);
         $paciente = Paciente::fromArray($resultado);
         return $resultado;
+    }*/
+
+    public static function ObtenerPacientes($id): ?Paciente{
+        try{
+            $conexion = new PDO("mysql:host=mariadb;dbname=examen", "alumno", "alumno");
+            $conexion->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        }catch(PDOException $e){
+            echo "Error: ".$e->getMessage();
+        }
+        $sql = "SELECT * FROM pacientes WHERE id_paciente = :id";
+        $stmt = $conexion->prepare($sql);
+        $stmt->bindParam(":id",$id);
+        $stmt->execute();
+        $resultado = $stmt->fetch(PDO::FETCH_ASSOC);
+        return self::fromArray($resultado);
     }
 
     public static function fromArray(array $datos):Paciente{
@@ -60,7 +77,7 @@ class PacienteModelo
         }
     }
 
-    public function buscarPaciente(int $id): ?Paciente
+    /*/public function buscarPaciente(int $id): ?Paciente
     {
         for ($i = 0; $i < count($this->pacientes); $i++) {
             if ($this->pacientes[$i]->getId() === $id) {
@@ -68,6 +85,29 @@ class PacienteModelo
             }
         }
         return null;
-    }
+    }*/
 
+    public static function buscarPaciente(int $id): ?Paciente
+    {
+        try{
+            $conexion = new PDO("mysql:host=mariadb;dbname=examen", "alumno", "alumno");
+            $conexion->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+            $sql = "SELECT * FROM pacientes WHERE id_paciente = :id";
+            $stmt = $conexion->prepare($sql);
+            $stmt->bindParam(":id",$id);
+            $stmt->execute();
+            $resultado = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            if (!$resultado) {
+                return null;
+
+            }
+            return self::fromArray($resultado);
+
+        }
+        catch (PDOException $e) {
+            die("Error en buscarPaciente: " . $e->getMessage());
+        }
+
+    }
 }
